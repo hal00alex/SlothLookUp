010f24a85c6e7697467300281bdcdd7be9473e1c
hal00alex <hal.00.alexander@gmail.com>
2016-04-17 08:08:10 -0700

    The Code

---
+++ Zoo.py
@@ -0,0 +1,19 @@
+import time 
+def main ():
+    char1 = input ('First Character of Plate: ')
+    time.sleep(60)
+    char2 = input ('\nSecond Character of Plate: ')
+    time.sleep(60)
+    char3 = input ('\nThird Character of Plate: ')
+    time.sleep(60)
+    char4 = input ('\nFourth Character of Plate: ')
+    time.sleep(60)
+    char5 = input ('\nFifth Character of Plate: ')
+    time.sleep(60)
+    char6 = input ('\nSixth Character of Plate: ')
+    time.sleep(60)
+    char7 = input ('\nSeventh Character of Plate: ')
+    time.sleep(120)
+    print("It is at the limo company") 
+
+main () 
